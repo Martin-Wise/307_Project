a7bb7eeebe4197f8be50c8369c34de78c74fd176
Martin Wise <mmwise2013@gmail.com>
2024-04-19 19:31:44 -0700

    updated backend.js

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -21,12 +21,24 @@ app.get("/users", (req, res) => {
     }
   });
 
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 app.listen(port, () => {
     console.log(
         `Example app listening at http://localhost:${port}`
     );
 });
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 const findUserByName = (name) => {
     return users["users_list"].filter(
